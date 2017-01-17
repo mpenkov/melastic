7b667516dc608dd90109e92b3df9bb01f4974e63
Michael Penkov <misha.penkov@gmail.com>
2017-01-17 14:40:43 +1100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(
-    zip_safe=False,
     name="melastic",
     version="0.0.1",
     author="Michael Penkov",
     author_email="misha.penkov@gmail.com",
     packages=["melastic"],
     description="Performs ElasticSearch bulk and scroll tasks",
-    url="https://github.com/mpenkov/melastic"
+    url="https://github.com/mpenkov/melastic",
+    download_url="https://github.com/mpenkov/melastic/tarball/v0.0.1",
+    keywords=["elastic"],
+    classifiers=[]
 )
